cc5842260fd5841bc37d4bbe3a8e52b59f2aea6b
juaosuna <jdosuna4@misena.edu.co>
2022-04-08 21:32:27 -0500

    actualización

---
+++ suma de pares.py
@@ -0,0 +1,12 @@
+print("ingresar el numero inicial")
+i = int(input("ingrese el numero inicial"))
+print("ingrese el numero final")
+f = int(input("ingrese el numero final"))
+suma = 0
+print("**los numeros pares del rango**")
+while i <= f:
+    if i % 2 == 0:
+        print(i)
+    i+=1
+    suma=suma+1
+print("la suma de los numeros es:", suma)
